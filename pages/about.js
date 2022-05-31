d28d2f08c5e850082da787edc2da51359311a235
t-nakai10 <bad.sideline.999@gmail.com>
2022-05-31 17:06:27 +0900

    layout 問題が解決できないため削除

--- pages/about.js
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/layout';
-
-export default function About() {
-  return <h1>About Page</h1>
-}
-
-About.getLayout = function getLayout(page) {
-  return <Layout>{page}</Layout>
-};
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 import Head from "next/head";
 
 export default function Home() {
