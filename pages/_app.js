c710a569030084e6e4211ecc9566b63512d2d7ea
t-nakai10 <bad.sideline.999@gmail.com>
2022-05-31 16:58:10 +0900

    使用してないレイアウトの設定削除

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '../styles/globals.css';
-import Layout from '../components/layout';
 
 export default function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
